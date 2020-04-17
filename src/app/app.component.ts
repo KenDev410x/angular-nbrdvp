364a04027556a602e2b8244cba0f593a2566b3b1
KenDev410x <sittisak.ken@gmail.com>
2020-04-17 17:48:28 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,7 @@ export class AppComponent implements OnInit {
   url: string = "https://jsonplaceholder.typicode.com/posts";
   items: any = [];
   constructor(private http: HttpClient) {}
-  // constructor() {}
-  //// callback
+  // // callback
   // ngOnInit() {
   //   console.log('1 first start...');
   //   this.asyncfuction(()=>{
@@ -31,28 +30,27 @@ export class AppComponent implements OnInit {
   // }
 
   ////  who to async await
-  // items: any = [];
-  //   async ngOnInit() {
-  //     console.log("1 first start...");
-  //     await this.asyncFuction();
-  //     console.log("3 first start...");
-  //   }
-  //   syncFunction() {
-  //     confirm("confirm !!!");
-  //     console.log("2 sync Function");
-  //   }
-  //   asyncFuction() {
-  //     return new Promise(resolve => {
-  //       setTimeout(() => {
-  //         console.log("2 Async Function");
-  //         resolve();
-  //       }, 100);
-  //     });
-  //   }
+    // async ngOnInit() {
+    //   console.log("1 first start...");
+    //   await this.asyncFuction();
+    //   console.log("3 first start...");
+    // }
+    // syncFunction() {
+    //   confirm("confirm !!!");
+    //   console.log("2 sync Function");
+    // }
+    // asyncFuction() {
+    //   return new Promise(resolve => {
+    //     setTimeout(() => {
+    //       console.log("2 Async Function");
+    //       resolve();
+    //     }, 100);
+    //   });
+    // }
+  //////////////////
   ngOnInit() {
     this.getPosts();
   }
-
   async getPosts() {
    this.items = await this.http.get(this.url).toPromise();
     console.log(this.items);
@@ -69,4 +67,5 @@ export class AppComponent implements OnInit {
       }, 100);
     });
   }
+
 }
